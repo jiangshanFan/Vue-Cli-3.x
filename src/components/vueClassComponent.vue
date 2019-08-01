f047aa2eb2cc6ae8c395003685a58ca35c57eec3
jiangshan.fan <745989449@qq.com>
2019-08-01 16:39:20 +0800

    init

---
+++ src/components/vueClassComponent.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <label for="hello">标题：</label>
+    <input id="hello" v-model="msg">
+    <p>prop: {{propMessage}}</p>
+    <p>msg: {{msg}}</p>
+    <p>helloMsg: {{helloMsg}}</p>
+    <p>computed msg: {{computedMsg}}</p>
+    <button @click="greet">Greet</button>
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue'
+  import Component from 'vue-class-component'
+
+  @Component({
+    props: {
+      propMessage: String
+    }
+  })
+  export default class VueClassComponent extends Vue {
+    // initial data
+    msg = 123
+
+    // use prop values for initial data
+    helloMsg = 'Hello, ' + this.propMessage
+
+    // lifecycle hook
+    mounted () {
+      this.greet()
+    }
+
+    // computed
+    get computedMsg () {
+      return 'computed ' + this.msg
+    }
+
+    // method
+    greet () {
+      alert('greeting: ' + this.msg)
+    }
+  }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,53 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
+<!-- ======================= can't be anything of template -->
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Anchored from '../components/HelloWorld.js'
+import VueClassComponent from '../components/vueClassComponent'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  name: 'Home',
+  methods: {
+    hello() {
+      console.log('hello');
+    },
+  },
+  render (h) {  //=============== vOn:click={self.hello} 必须放在当前的组件环境下才能生效，如果放在引用的组件中，必须通过props传递，触发响应组件的方法
+    let self = this;
+    const attr = {
+      level: self.level,
+      class: 'bg'
+    }
+
+    return (
+      <div vOn:click={self.hello}>
+        {
+          Array.apply(null, { length: 3 }).map(function () {
+            return (
+              <Anchored {...{ attrs: attr }}>
+                <p>Hello, <span>{ self.message }</span> right? <span domPropsInnerHTML={self.html}>nice!</span></p>
+                <header slot="header">header</header>
+                <footer slot="footer">footer</footer>
+              </Anchored>
+            )
+          })
+        }
+        <VueClassComponent propMessage="vueClassComponentTest" />
+      </div>
+    )
+  },
+
+  data() {
+    return {
+      level: 1,
+      html: 'a rainy day!',
+      message: 'a message!'
+    }
   }
 }
 </script>
+
+<style lang="scss">
+  .bg {
+    background-color: #f5f5f5;
+  }
+</style>
